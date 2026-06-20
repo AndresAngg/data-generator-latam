44affa533f1c0c9e71be92ce3754072b2871ca52
AndresAngg <andresangg@gmail.com>
2026-06-19 22:22:21 -0500

    Add user generation, listing, and CSV export functionality

--- index.ts
+++ index.ts
@@ -1,9 +1,54 @@
 import { DataGeneratorService } from "./src/services/DataGeneratorService";
+import { UserRepository } from "./src/database/UserRepository";
+import { CsvExportService } from "./src/services/CsvExportService";
 
-const generator =
-    new DataGeneratorService();
+async function main() {
 
-const users =
-    generator.generateUsers(80);
+    const action = process.argv[2];
+    const repository = new UserRepository();
 
-console.table(users);
+    switch (action) {
+        case "generate":
+
+            const quantity = Number(process.argv[3]) || 10;
+            const generator = new DataGeneratorService();
+            const users = generator.generateUsers(quantity);
+
+            for (const user of users) {
+                await repository.save(user);
+            }
+
+            const csv = new CsvExportService();
+            await csv.export(users);
+
+            console.log(`${quantity} usuarios generados`);
+
+            break;
+
+        case "list":
+
+            const allUsers = await repository.getAll();
+            console.table(allUsers);
+
+            break;
+
+        case "clean":
+
+            await repository.deleteAll();
+            console.log("Base de datos limpiada");
+
+            break;
+
+        default:
+            console.log(`
+Comandos disponibles:
+
+npm run start generate 100
+npm run start list
+npm run start clean`);
+
+    }
+
+}
+
+main();
--- src/database/UserRepository.ts
+++ src/database/UserRepository.ts
@@ -46,4 +46,90 @@ export class UserRepository {
 
     }
 
+    getAll(): Promise<any[]> {
+
+        return new Promise((resolve, reject) => {
+
+            this.db.all(
+                `
+                SELECT
+                    id,
+                    name,
+                    lastName,
+                    age,
+                    document,
+                    city,
+                    country,
+                    language,
+                    type
+                FROM users
+                `,
+                [],
+                (err, rows) => {
+                    if (err) reject(err);
+                    else resolve(rows || []);
+                }
+            );
+
+        });
+
+    }
+
+    findByDocument(document: string): Promise<any> {
+
+        return new Promise((resolve, reject) => {
+
+            this.db.get(
+                "SELECT * FROM users WHERE document = ?",
+                [document],
+                (err, row) => {
+
+                    if (err) reject(err);
+                    else resolve(row);
+
+                }
+            );
+
+        });
+
+    }
+
+    deleteByDocument(document: string): Promise<void> {
+
+        return new Promise((resolve, reject) => {
+
+            this.db.run(
+                "DELETE FROM users WHERE document = ?",
+                [document],
+                (err) => {
+
+                    if (err) reject(err);
+                    else resolve();
+
+                }
+            );
+
+        });
+
+    }
+
+    deleteAll(): Promise<void> {
+
+        return new Promise((resolve, reject) => {
+
+            this.db.run(
+                "DELETE FROM users",
+                [],
+                (err) => {
+
+                    if (err) reject(err);
+                    else resolve();
+
+                }
+            );
+
+        });
+
+    }
+
 }
