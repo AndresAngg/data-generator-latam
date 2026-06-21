6d354d12b58c7442ccdf11698ce7534f05a3099e
AndresAngg <andresangg@gmail.com>
2026-06-21 09:22:23 -0500

    Refactor main function to improve code readability and maintainability

--- index.ts
+++ index.ts
@@ -5,42 +5,36 @@ import { EmailService } from "./src/services/EmailService";
 import dotenv from "dotenv";
 
 async function main() {
-
     dotenv.config();
     const action = process.argv[2];
     const repository = new UserRepository();
 
     switch (action) {
         case "generate":
-
             const quantity = Number(process.argv[3]) || 10;
             const generator = new DataGeneratorService();
             const users = generator.generateUsers(quantity);
-
-            await repository.save(users);
-
             const csv = new CsvExportService();
-            await csv.export(users);
-
             const emailService = new EmailService();
 
+            await repository.save(users);
+            await csv.export(users);
+            
             console.log(`${quantity} usuarios generados`);
+            
             await emailService.sendCsv();
-
             break;
 
         case "list":
-
             const allUsers = await repository.getAll();
+            
             console.table(allUsers);
-
             break;
 
         case "clean":
-
             await repository.deleteAll();
-            console.log("Base de datos limpiada");
 
+            console.log("Base de datos limpiada");
             break;
 
         default:
@@ -50,9 +44,7 @@ Comandos disponibles:
 npm run start generate 100
 npm run start list
 npm run start clean`);
-
     }
-
 }
 
 main();
